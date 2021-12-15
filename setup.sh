a746fc2c95d5a541ec808005f74b911688a7d6c4
Rafael Mursalov <rafael.mursalov@ibar.az>
2021-12-15 13:02:03 +0400

    add missing setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# Create virtual environment
+python3 -m venv ./venv
+
+# Install pyinstaller
+./venv/bin/pip3 install -r ./requirements.txt
+
+# Package xctest.py
+./venv/bin/pyinstaller ./xctest.py --onedir --clean -y --distpath ./packaged --workpath ./packaged/build --specpath ./packaged
+
+# Remove pycache
+rm -rf ./__pycache__
